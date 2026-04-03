3daa2d1b9a57a38e6bdaf5b1456ad07847827fa2
Velraaj <velraaj30@gmail.com>
2026-04-04 03:04:56 +0530

    Updated authservice

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,11 +2,11 @@ import type { User } from "../types/User";
 import api from "./api";
 
 export const authService = {
-    login : (data : User) => api.post("/login", data ),
+    login : (data : User) => api.post("/auth/login", data ),
 
-    register : (data: User) => api.post("/register", data),
+    register : (data: User) => api.post("/auth/register", data),
 
-    logout : () => api.post("/logout"),
+    logout : () => api.post("/auth/logout"),
 
-    refreshToken : (data : string) => api.post("/refreshToken", data) 
+    refreshToken : (data : string) => api.post("/auth/refreshToken", data) 
 }
